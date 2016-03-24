16fa7ba9c0750dbfd2a01f9beadc233a6a7218f1
Alex D <broscutamaker@gmail.com>
2016-03-25 01:52:16 +0200

    hdmi4k: updated comments in render script

--- raw-via-hdmi/hdmi4k/render.sh
+++ raw-via-hdmi/hdmi4k/render.sh
@@ -1,9 +1,18 @@
-# argument: mov file
+#!/bin/sh
+# Convert Shogun MOV footage recorded with Axiom BETA to DNG
+
+# Input should be a .mov file transcoded from Shogun with ffmpeg -vcodec copy (not directly!)
+# that's because I've designed the HDMI recovery filters on transcoded output 
+# which is - surprise! - not identical (yes, with -vcodec copy; ffmpeg bug?)
+
+# You need to get calibration frames (darkframe-hdmi-[AB].ppm) from:
+# http://files.apertus.org/AXIOM-Beta/snapshots/calibration-frames/cam2/
 
 # careful here
 rm frame*.dng
 
+# this reads the MOV file via ffmpeg
+# the output is piped to raw2dng, which also does temporal row noise correction
 hdmi4k $1 - | raw2dng --pgm --fixrnt frame%05d.dng
 
-# todo:
-# - temporal row noise correction
+# todo: HDMI dark frame averaging script
