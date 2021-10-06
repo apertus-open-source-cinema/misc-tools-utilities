e976ebd362abc2359f236c1d72b6299c784c3225
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2021-10-06 21:15:21 +0200

    updates

--- raw-via-hdmi/scripts/bgr-convert.py
+++ raw-via-hdmi/scripts/bgr-convert.py
@@ -7,36 +7,37 @@ previousframecounter = 0
 first = True
 frameindex = 0
 fileindex = 0
-version = 'V0.01'
+version = 'V0.2'
 resolution_width = 1920
 resolution_height = 1080
-target = 'raw12 & dng'
+target = 'raw12&dng'
 
 
 def print_help():
     print('This is bgr-convert ' + version)
     print('')
     print('SYNOPSIS')
-    print('\tbgr-convert.py [parameters] input-folder')
+    print('\tbgr-convert.py [parameters]')
     print('')
     print('EXAMPLE')
-    print('\tbgr-convert.py --width=2048 --height=1080 myfolder/')
+    print('\tbgr-convert.py -w 2048 -h 1080 -i myfolder/')
     print('')
-    print('PARAMETERS')
+    print('OPTIONS')
     print('\t-w, --width:\t defines image resolution width (default: 1920)')
     print('\t-h, --height:\t defines image resolution height (default: 1080)')
     print('\t-i, --input:\t path to files that should be converted')
-    print('\t-t, --target:\t options are: raw12, dng, raw12 & dng (default: raw12 & dng)')
+    print('\t-t, --target:\t options are: raw12, dng, raw12&dng (default: raw12&dng)')
 
 
 def main(argv):
     global folder, resolution_height, resolution_width, target
+
     try:
-        opts, args = getopt.getopt(argv, "d:h:w:t:i:t:",
-                                   ["help", "width", "height", "target", "input", "target"])
+        opts, args = getopt.getopt(argv, "h:w:h:t:i:", ["help", "width=", "height=", "target=", "input="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
+
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print_help()
@@ -47,13 +48,13 @@ def main(argv):
         elif opt in ("-h", "--height"):
             resolution_height = arg.strip()
             print('Parameter provided: Using resolution height: ' + resolution_height)
-        elif opt in ("-i", "--input"):
-            folder = arg.strip()
-            print('Converting input-folder: ', folder)
         elif opt in ("-t", "--target"):
             target = arg.strip()
             print('Target: ', target)
-
+        elif opt in ("-i", "--input"):
+            folder = arg.strip()
+        else:
+            print('Else: ', arg.strip())
 
 if __name__ == "__main__":
     main(sys.argv[1:])
@@ -61,15 +62,24 @@ if __name__ == "__main__":
 if (folder == ''):
     print('No input-folder specified')
     print('Exiting...')
+    print()
+    print_help()
     sys.exit()
 
-
+stream = os.popen('ls ' + folder + '*.bgr | wc -l')
+filescount = stream.read().strip('\n')
 clipname = folder
 
-
-stream = os.popen('ls ' + folder + '*.bgr | wc -l')
-filescount = stream.read()
-print('bgr files found in folder: ' + filescount)
+print('Configuration:')
+print('')
+print('folder:\t\t\t\t', folder)
+print('bgr files found in folder:\t' + filescount)       
+print('target:\t\t\t\t' + target)
+print('source resolution width:\t' + str(resolution_width))
+print('source resolution height:\t' + str(resolution_height))
+print('target resolution width:\t' + str(resolution_width*2))
+print('target resolution height:\t' + str(resolution_height*2))
+print('')
 
 filenamelist = os.listdir(folder + '.')
 filenamelist.sort()
@@ -95,11 +105,12 @@ for filename in filenamelist:
 
             # convert every pair if an A-Frame exists
             if (abframe == "A-Frame"):
+
                 # extract framecounter
                 stream = os.popen('dd if=' + folder + filenamelist[fileindex+1] + ' bs=1 count=1 skip=2 status=none | od -An -vtu1')
                 framecounternext = int(stream.read().strip("\n"))
 
-                #only continue if a B frame exists that has a one value higher framecounter  
+                #only continue if a B frame exists that contains a framecounter that is one value highe than the A-frame
                 if ((framecounternext == framecounter + 1) | ((framecounternext == 0) & (framecounter == 255))):
                     print(str(framecounter) + ':\t' + folder + filenamelist[fileindex] + '\t&\t' + str(framecounternext) + ':\t' + folder + filenamelist[fileindex+1] +
                           '\t-> ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12')
@@ -110,16 +121,20 @@ for filename in filenamelist:
                     print(stream.read())
 
                     # write dng
-                    print            ('raw2dng ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12 --width=' + str(resolution_width*2) + ' --height=' + str(resolution_height*2))
-                    stream = os.popen('raw2dng ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12 --width=' + str(resolution_width*2) + ' --height=' + str(resolution_height*2))
-                    print(stream.read())
+                    if (target != "raw12"):
+                        print            ('raw2dng ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12 --width=' + str(resolution_width*2) + ' --height=' + str(resolution_height*2))
+                        stream = os.popen('raw2dng ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12 --width=' + str(resolution_width*2) + ' --height=' + str(resolution_height*2))
+                        print(stream.read())
 
                     # remove raw12
-                    #print('rm ' + clipname + f'{frameindex:05}' + '.raw12')
-                    #stream = os.popen('rm ' + clipname + f'{frameindex:05}' + '.raw12')
-                    #stream.read()
+                    if (target == "dng"):
+                        print('rm ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12')
+                        stream = os.popen('rm ' + folder + clipname.strip('/') + f'_{frameindex:05}' + '.raw12')
+                        stream.read()
+                        print('')
 
                     frameindex = frameindex + 1
+
             previousframecounter = framecounter
             #frameindex = frameindex + 1
         fileindex = fileindex + 1
