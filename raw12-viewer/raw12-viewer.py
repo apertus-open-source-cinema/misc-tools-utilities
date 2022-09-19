833016a976e962f23cfb7901348e779b267d3d52
Andrej <b.andrej@cumir.de>
2022-09-19 21:57:19 +0200

    T1276: Fixed image switching

--- raw12-viewer/raw12-viewer.py
+++ raw12-viewer/raw12-viewer.py
@@ -173,14 +173,14 @@ def main_loop():
 
         elif event == 'Right:114' or event == '-next-image-':
             current_image_index += 1
-            if current_image_index == file_list_length - 1:
-                current_image_index = current_image_index
+            if current_image_index > file_list_length - 1:
+                current_image_index = file_list_length - 1
             handle_image_switching(current_image_index, image_dir, raw12_file_list, window)
             pass
 
         elif event == 'Left:113' or event == '-previous-image-':
             current_image_index -= 1
-            if current_image_index == 0:
+            if current_image_index < 0:
                 current_image_index = 0
             handle_image_switching(current_image_index, image_dir, raw12_file_list, window)
             pass
@@ -219,9 +219,10 @@ def main_loop():
             handle_image_dragging(event, values)
 
 
-def handle_image_switching(current_image_index, image_dir, raw12_file_list, window):
+def handle_image_switching():
+    global current_image_index, image_dir, raw12_file_list, window
     file_name = raw12_file_list[current_image_index]
-    window.set_title('raw12 Viewer: ' + file_name)
+    window.set_title('raw12 Viewer: ' + str(file_name))
     update_next_image_buttons()
     load_image(Path(image_dir, file_name))
     show_images()
